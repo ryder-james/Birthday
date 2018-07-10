913c11e325f2fe6777a49ac4959ace1be4872059
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-10 12:02:41 -0600

    Added button rendering capability

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,25 @@ import Button from './button';
 import Clock from './clock';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clockActive: false
+    }
+  }
+
+  renderItems = function() {
+    if(this.state.clockActive) {
+      return [
+        <Clock/>,
+        Button("Change Date", () => this.setState({clockActive: false}))
+      ]
+    } else {
+      return Button("Generate Countdown", () => this.setState({clockActive: true}));
+    }
+  }.bind(this)
+
   render() {
     return (
       <div className="grid">
@@ -13,9 +32,7 @@ export default class App extends Component {
         <div className="grid__skew-light" />
 
         <Picker/>
-        { Button("Generate Countdown") }
-
-        <Clock/>
+        { this.renderItems() }
       </div>
     );
   }
