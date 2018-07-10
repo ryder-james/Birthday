8a6342dca6f02e9e1fc73b729492578d6d314558
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-10 10:53:14 -0600

    Added button

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Picker from './picker';
+import Button from './button';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <div className="grid__skew-light" />
 
         <Picker/>
+        { Button("Generate Countdown") }
       </div>
     );
   }
