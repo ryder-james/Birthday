e20387f5f81373fd5f3c485a409c6fca3faa105a
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-10 09:49:53 -0600

    Started work on DatePicker

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Picker from './picker';
 
 export default class App extends Component {
   render() {
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
+        
         <div className="grid__skew-dark"></div>
         <div className="grid__skew-light" />
+
+        <Picker/>
       </div>
     );
   }
